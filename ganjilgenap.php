ce06e824eb2b94f250edfe614a36dd15a1385dba
abangmuslim <abangmuslimtamiang@gmail.com>
2024-08-10 12:18:21 +0700

    memasukkan file percepatan dan contoh project

---
+++ ganjilgenap.php
@@ -0,0 +1,13 @@
+plaintext
+<php>
+    
+</php>
+Salin kode
+START
+   INPUT angka
+   IF angka MOD 2 = 0 THEN
+      OUTPUT "Bilangan Genap"
+   ELSE
+      OUTPUT "Bilangan Ganjil"
+   ENDIF
+END
---
+++ hitung percepatan.php
@@ -0,0 +1,32 @@
+
+<?php
+
+// Mulai
+// Deklarasi, jarak, waktu, kecepatan, percepatan
+$s=100;
+$t=1.5;
+
+// Tentukan nilai jarak
+// Tentukan nilai waktu
+echo "jarak =". $s;
+echo "<br>waktu =". $t;
+
+// Bagikan nilai jarak dengan waktu
+$v=$s/$t;
+
+// Tampilkan hasil pembagian
+echo "<br>hasil kecepatan adalah =". $v;
+
+// Bagikan nilai kecepatan dengan waktu
+$a=$v/$t;
+
+// Tampilkan hasil pembagian
+echo "<br> hasil percepatan adalah =". $a;
+
+
+
+
+
+
+
+
