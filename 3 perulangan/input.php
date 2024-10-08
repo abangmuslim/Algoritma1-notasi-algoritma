472e54e870da92badc934140489955f6592a090b
abangmuslim <abangmuslimtamiang@gmail.com>
2024-10-08 21:16:27 +0700

    memperbaiki perulangan dengan dropdown

--- 3 perulangan/input.php
+++ 3 perulangan/input.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Perulangan PHP</title>
 </head>
+
 <body>
     <form method="POST" action="output.php">
-        <label for="jumlah">Masukkan jumlah pengulangan:</label><br>
-        <input type="number" id="jumlah" name="jumlah" min="1" required><br><br>
-        <input type="submit" value="Submit">
+        <label for="jumlah">Pilih berapa kali akan diulang:</label><br><br>
+        <select id="jumlah" name="jumlah" min="1" required>
+            <!-- Loop jumlah 1 sampai 100 -->
+            <!-- Kamu juga bisa menggunakan script untuk mengisi jumlah secara otomatis -->
+            <option value=""> --- Pilih Jumlah ---</option>
+            <!-- Pilihan jumlah dari 1 hingga 100 -->
+            <script>
+                for (let i = 1; i <= 100; i++) {
+                    document.write('<option value="' + i + '">' + i + '</option>');
+                }
+            </script>
+        </select>
+        <input type="submit" value="Proses ->">
     </form>
 
-    
+
 </body>
+
 </html>
--- 3 perulangan/output.php
+++ 3 perulangan/output.php
@@ -4,17 +4,18 @@ $jumlah = $_POST['jumlah'];
 
 // Perulangan untuk menampilkan "Nama saya Ahmadi"
 for ($i = 1; $i <= $jumlah; $i++) {
-      echo "Nama saya Ahmadi<br>";
+      echo "Nama saya Ahmadi ke " . $i . "<br>";
 }
 ?>
 
 <p>
-<a href="input.php">
-      <button>Kembali ke Input</button>
-</a>
+      <a href="input.php">
+            <button>Kembali ke Input</button>
+      </a>
 </p>
 
-<p>atau kembali ke : </p>
+<p>atau kembali ke : 
 <a href="http://localhost/">
       <button>Localhost</button>
 </a>
+</p>
